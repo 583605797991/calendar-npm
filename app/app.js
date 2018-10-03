b3a294df1755ce60afb475c0f09f1de0fd8ccfed
Henrique Eidt <henriqueidt@gmail.com>
2018-10-03 19:00:53 -0300

    checkboxes added and some little changes

--- app/app.js
+++ app/app.js
@@ -6,9 +6,25 @@ $('#calendar').fullCalendar({
      columnHeaderText: function(moment) {
         if (moment.weekday() === 0) {
             return 'S'
+        } else if (moment.weekday() === 1) {
+            return 'M'
+        } else if (moment.weekday() === 2 || moment.weekday() === 4) {
+            return 'T'
+        } else if (moment.weekday() === 3) {
+            return 'W'
+        } else if (moment.weekday() === 5) {
+            return 'F'
+        } else {
+            return 'S'
         }
-
-        return 'M'
     } 
   });
+
+  $(".my-calendars").on('click', () => {
+      $(".mine").toggle('fast')
+  })
+
+  $(".other-calendars").on('click', () => {
+    $(".other").toggle('fast')
+})
 });
