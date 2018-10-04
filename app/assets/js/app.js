0153a6755c59920f45de9058a68c66a320aeb068
Henrique Eidt <henriqueidt@gmail.com>
2018-10-04 16:36:20 -0300

    adicionada funcionalidade de adicionar calendarios

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,68 +1,49 @@
 $(() => {
 
-<<<<<<< HEAD
-  $('#mini-calendar').fullCalendar({
-      defaultView: 'month',
-      height: 200,
-      columnHeaderText: function(moment) {
-          if (moment.weekday() === 0) {
-              return 'S'
-          } else if (moment.weekday() === 1) {
-              return 'M'
-          } else if (moment.weekday() === 2 || moment.weekday() === 4) {
-              return 'T'
-          } else if (moment.weekday() === 3) {
-              return 'W'
-          } else if (moment.weekday() === 5) {
-              return 'F'
-          } else {
-              return 'S'
-          }
-      }
-    });
-
-    $("input[class=input]").on('keyup', function(event) {
-      if (event.keyCode === 13) {
-        console.log('pressionou enter');
-=======
     $('#mini-calendar').fullCalendar({
         defaultView: 'month',
         height: 200,
         columnHeaderText: function (moment) {
-            if (moment.weekday() === 0) {
+          var day = moment.weekday();
+            if (day === 0) {
                 return 'S'
-            } else if (moment.weekday() === 1) {
+            } else if (day === 1) {
                 return 'M'
-            } else if (moment.weekday() === 2 || moment.weekday() === 4) {
+            } else if (day === 2 || day === 4) {
                 return 'T'
-            } else if (moment.weekday() === 3) {
+            } else if (day === 3) {
                 return 'W'
-            } else if (moment.weekday() === 5) {
+            } else if (day === 5) {
                 return 'F'
             } else {
                 return 'S'
             }
->>>>>>> master
         }
+    });
+
+    $("input[class=input]").on('keyup', function(event) {
+      if (event.keyCode === 13) {
+        var textVal = $(".input").val();
+        `testeasdka sodkas dçasm kdnasç ndçaksn dçaksnd ${textVal}`
+        $('.other').append(`
+        <li>
+          <div class="field">
+            <input
+              class="is-checkradio has-background-color is-link"
+              id="exampleCheckboxBackgroundColorLink"
+              type="checkbox"
+              name="exampleCheckboxBackgroundColorLink"
+              checked="checked">
+            <label for="exampleCheckboxBackgroundColorLink">
+              ${textVal}
+            </label>
+          </div>
+        </li>`)
+      }
     });
 
     $(".my-calendars").on('click', () => {
         $(".mine").toggle('fast')
-<<<<<<< HEAD
-    })
-
-    $(".other-calendars").on('click', () => {
-      $(".other").toggle('fast')
-  });
-
-  $(() => {
-    $('#calendar').fullCalendar({
-        defaultView: 'agendaWeek',
-        contentHeight: 888
-    })
-  }) ;
-});
-=======
     })
 
     $(".other-calendars").on('click', () => {
@@ -79,4 +60,3 @@ $(() => {
     });
 
 });
->>>>>>> master
