30abc6e0b7b505ff3c209afe0673590590d12576
Henrique Eidt <henriqueidt@gmail.com>
2018-10-04 15:14:53 -0300

    arquivo app.js fora da pasta js removido

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,6 +1,43 @@
 $(() => {
+
+  $('#mini-calendar').fullCalendar({
+      defaultView: 'month',
+      height: 200,
+      columnHeaderText: function(moment) {
+          if (moment.weekday() === 0) {
+              return 'S'
+          } else if (moment.weekday() === 1) {
+              return 'M'
+          } else if (moment.weekday() === 2 || moment.weekday() === 4) {
+              return 'T'
+          } else if (moment.weekday() === 3) {
+              return 'W'
+          } else if (moment.weekday() === 5) {
+              return 'F'
+          } else {
+              return 'S'
+          }
+      }
+    });
+
+    $("input[class=input]").on('keyup', function(event) {
+      if (event.keyCode === 13) {
+        console.log('pressionou enter');
+        }
+    });
+
+    $(".my-calendars").on('click', () => {
+        $(".mine").toggle('fast')
+    })
+
+    $(".other-calendars").on('click', () => {
+      $(".other").toggle('fast')
+  });
+
+  $(() => {
     $('#calendar').fullCalendar({
         defaultView: 'agendaWeek',
         contentHeight: 888
     })
-})
+  }) ;
+});
