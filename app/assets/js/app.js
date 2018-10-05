5005c5d926bc6e4b3360de77d8dfd0fa6d02d878
Henrique Eidt <henriqueidt@gmail.com>
2018-10-05 11:52:44 -0300

    Adicionada funcionalidade ao cliar no evento

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -79,6 +79,14 @@ $('.calendar').fullCalendar({
     $('.calendar').fullCalendar('addEventSource', eventSource);
   },
 
+  eventClick: function(calEvent, jsEvent, view) {
+    alert('Event: ' + calEvent.title);
+    alert('Event start: ' + calEvent.start.format('MMMM Do YYYY, h:mm:ss a'));
+    alert('Event end: ' + calEvent.end.format('MMMM Do YYYY, h:mm:ss a'));
+    $(this).css('border-color', 'red');
+
+  },
+
 });
 
 const addCalendar = (calendar) => {
